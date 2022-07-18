30084706e7dcbeeac13c7149dcbb5b256f6c673c
spore310 <yohansabigail@yahoo.com>
2022-07-18 01:50:16 -0400

    added next/image

--- components/SSRP/CharacterCard.tsx
+++ components/SSRP/CharacterCard.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import styles from './CharacterCard.module.scss'
-
+import Image from 'next/image';
 const CharacterCard = ({char, routeTo}:any) =>{
 //id: ele.id, name:ele.name, image:ele.image, status: ele.status,species:ele.species
     return(
         <div className={styles.card}>
-            <section>
-                <img className={styles.image} src={char.image}/>
+            <section >
+                <Image className={styles.image} src={char.image}  height={300} width={300} quality={100}/>
             </section>
             <section className={styles["card-info"]}>
-                <p>{char.name}</p>
-                <p>{char.status}</p>
-                <p>{char.species}</p>
+                <p>Name:  {char.name}</p>
+                <p>Status:  {char.status}</p>
+                <p>Species:  {char.species}</p>
                 
             </section>
         </div>
