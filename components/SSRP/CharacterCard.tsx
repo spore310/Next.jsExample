63a52f5327ec3524bef0016a8ff8471f8dee3f38
spore310 <yohansabigail@yahoo.com>
2022-07-21 02:52:23 -0400

    online rebase to local

--- components/SSRP/CharacterCard.tsx
+++ components/SSRP/CharacterCard.tsx
@@ -6,15 +6,9 @@ const CharacterCard = ({char, routeTo}:any) =>{
 //id: ele.id, name:ele.name, image:ele.image, status: ele.status,species:ele.species
     return(
         <div className={styles.card}>
-<<<<<<< HEAD
-            <div className={styles.imageBorder}>
-                <Image className={styles.image} src={char.image} layout='responsive' height={256} width={300}  quality={100}/>
-            </div>
-=======
             <section className={char.status=="Alive" ? "" : styles["not-alive-img"]}>
                 <Image className={styles.image} src={char.image}  height={300} width={300} quality={100}/>
             </section>
->>>>>>> 0b833f86787fd146cac9d6a1cb155457ee80ee7a
             <section className={styles["card-info"]}>
                 <h2 className={styles.name}>{char.name}</h2>
                 <div className={styles.status}>
