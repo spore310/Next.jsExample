d1fa2457442644c1c2ad9d7001c9189f79920123
Cooper Li <827076738cooper@gmail.com>
2022-07-26 18:53:42 -0500

    changed error messages. made some styles and layout change.

--- components/SSRP/CharacterList.tsx
+++ components/SSRP/CharacterList.tsx
@@ -1,90 +1,109 @@
 import { useQuery, gql } from "@apollo/client";
 import { RestaurantMenuRounded } from "@mui/icons-material";
-import React, { FC, useState } from 'react';
-import styles from './CharacterList.module.scss';
+import React, { FC, useState } from "react";
+import styles from "./CharacterList.module.scss";
 const Query = gql`
-    query GetCharacterList($pageNum: Int){
-        characters(page:$pageNum){
-            info{
-                next,
-                prev,
-                pages
-            }
-            results{
-                id,
-                name,
-                gender,
-                status,
-                species,
-                type,
-                image,
-                location{
-                    name,
-                    dimension
-                    },
-                origin{
-                    name,
-                    dimension
-                    },
-                
-
-            }
-        }  
+  query GetCharacterList($pageNum: Int) {
+    characters(page: $pageNum) {
+      info {
+        next
+        prev
+        pages
+      }
+      results {
+        id
+        name
+        gender
+        status
+        species
+        type
+        image
+        location {
+          name
+          dimension
+        }
+        origin {
+          name
+          dimension
+        }
+      }
     }
+  }
 `;
 
 export const CharacterList: FC = () => {
-    const [page, setPageNum] = useState(1);
-    const [numOfPages, setNumOfPages] = useState(1);
-    const { data, loading, error } = useQuery(Query, {
-        variables: { pageNum: page }
-    });
-    if (error) {
-        {/* handles in case of error */}
-        return (<div className={styles.buttonGroup}>{'error'}</div>)
+  const [page, setPageNum] = useState(1);
+  const [numOfPages, setNumOfPages] = useState(1);
+  const { data, loading, error } = useQuery(Query, {
+    variables: { pageNum: page },
+  });
+  if (error) {
+    {
+      /* handles in case of error */
     }
-    if (loading) {
-        {/* handles loading state */}
-        return (<div className={styles.buttonGroup}>
-            <div> 
-                <button disabled={true}>{'<'}</button>
-                {`${page}/${numOfPages}`}
-                <button disabled={true}>{'>'}</button>
-            </div>
-            <p>loading...</p>
-        </div>)
+    return <div className={styles.buttonGroup}>{"Error!" + error.message}</div>;
+  }
+  if (loading) {
+    {
+      /* handles loading state */
     }
+    return (
+      <div className={styles.buttonGroup}>
+        <div>
+          <button disabled={true}>{"<"}</button>
+          {`${page}/${numOfPages}`}
+          <button disabled={true}>{">"}</button>
+        </div>
+        <p>loading...</p>
+      </div>
+    );
+  }
 
-    if (data) {
-        {/* handles when data is sent back */}
-        const { characters: { info, results } } = data;
-        if (numOfPages !== info.pages) { setNumOfPages(prev => info.pages); }
-        const prevPage = info.prev ? info.prev : null;
-        const nextPage = info.next ? info.next : null;
-
-        const handleNextPage = () => {
-            if (info.next === null) {
-                return
-            } else {
-                setPageNum(prev => prev + 1);
-            }
-        }
-        const handlePrevPage = () => {
-            if (info.prev === null) {
-                return
-            } else {
-                setPageNum(prev => prev - 1)
-            }
-        }
-
-
-        console.log(info, results);
-
-        return (<div className={styles.buttonGroup}>
-            {prevPage ? <button onClick={handlePrevPage}>{'<'}</button> : <button disabled={true}>{'<'}</button>}
-            {`${page}/${info.pages}`}
-            {nextPage ? <button onClick={handleNextPage}>{'>'}</button> : <button disabled={true}>{'>'}</button>}
-        </div>)
+  if (data) {
+    {
+      /* handles when data is sent back */
     }
-    return (<></>)
-}
+    const {
+      characters: { info, results },
+    } = data;
+    if (numOfPages !== info.pages) {
+      setNumOfPages((prev) => info.pages);
+    }
+    const prevPage = info.prev ? info.prev : null;
+    const nextPage = info.next ? info.next : null;
+
+    const handleNextPage = () => {
+      if (info.next === null) {
+        return;
+      } else {
+        setPageNum((prev) => prev + 1);
+      }
+    };
+    const handlePrevPage = () => {
+      if (info.prev === null) {
+        return;
+      } else {
+        setPageNum((prev) => prev - 1);
+      }
+    };
+
+    console.log(info, results);
+
+    return (
+      <div className={styles.buttonGroup}>
+        {prevPage ? (
+          <button onClick={handlePrevPage}>{"<"}</button>
+        ) : (
+          <button disabled={true}>{"<"}</button>
+        )}
+        {`${page}/${info.pages}`}
+        {nextPage ? (
+          <button onClick={handleNextPage}>{">"}</button>
+        ) : (
+          <button disabled={true}>{">"}</button>
+        )}
+      </div>
+    );
+  }
+  return <></>;
+};
